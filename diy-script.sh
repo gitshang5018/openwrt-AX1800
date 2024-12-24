2f1cfc0aa242681866a2741a7a6fc219f4f424c8
gitshang5018 <61613626+gitshang5018@users.noreply.github.com>
2024-12-24 11:22:58 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -73,7 +73,7 @@ git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-a
 git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
 
 # MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns.git
 
 # Alist
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
